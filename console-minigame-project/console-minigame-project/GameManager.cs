32db054be802cfc221b0889e89115fda8d4120d2
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-03-19 14:19:08 +0900

    Update GameManager.cs

--- console-minigame-project/console-minigame-project/GameManager.cs
+++ console-minigame-project/console-minigame-project/GameManager.cs
@@ -12,12 +12,13 @@ class GameManager
     private List<string> Current_MineWords;
     private int TurnCount = 0;
     private string UserInput;
+    private bool isGameOver = false;
     public GameManager()
     {
 
     }
 
-    // 카테고리 선택
+    // 카테고리 선택, 랜덤 지뢰 생성
     public void SetCategory()
     {
         Random random = new Random();
@@ -33,37 +34,37 @@ class GameManager
             else
             {
                 // 현재 카테고리 단어들만 변수에 담아두기
-                // 지뢰 단어 랜덤 생성하기
+                // 지뢰 단어 n 개 랜덤 생성하기 
                 Current_Category = wordData.CategoryNames[num];
-                for (int i = 0; i < length; i++)
+                for (int i = 0; i < 3; i++)
                 {
-                    
+                    if (Current_MineWords.Contains(Current_Category[random.Next(1, Current_Category.Length)]))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        Current_MineWords.Add(Current_Category[random.Next(1, Current_Category.Length)]);
+                    }
                 }
-                Current_MineWords.Add(Current_Category[random.Next(1, Current_Category.Length)]);
-
-
-
             }
-
-
         }
-
     }
 
-    // 사용자 입력
+    // 사용자 단어 입력
     public void Input()   
     {
         //단어 유효성 검사 (null 일때, 저장된 단어리스트에 사용자 입력 단어가 없을때
         while (true)   
         {
-            Console.WriteLine("입력하세요");
+            Console.WriteLine("숨겨진 지뢰를 피해 단어를 입력해주세요!");
             string input = Console.ReadLine();
             if (string.IsNullOrEmpty(input))
             {
                 Console.WriteLine("단어를 다시 입력해주세요!");
                 continue;
             }
-            else if (!wordData.fruits.Contains(input.Trim()))
+            else if (!Current_Category.Contains(input.Trim()))
             {
                 Console.WriteLine("단어를 다시 입력해주세요!");
                 continue;
@@ -74,13 +75,21 @@ class GameManager
                 player.InputHistory.Add(UserInput);
                 break;
             }
-            wordData.CategoryName.v
         }
     }
 
-    public void MineWords(int count)
+    // 지뢰 검사 메서드
+    public void CheckIsMine()
     {
-        Current_MineWords
+        // 지뢰와 완전히 일치할 경우
+        if (Current_MineWords.Contains(UserInput))
+        {
+            Console.WriteLine("파방!! 지뢰가 터졌습니다. .");
+            isGameOver = true;
+            // 사망 시 행동할 로직
+        }
+        // 지뢰와 n 글자 일치할 경우
+        switch 
     }
 
 
